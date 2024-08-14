139f16953cf13249a0634de757ec149f3a0fbbdb
Firebirbionmahtavakoodaajees <kivasopo@outlook.com>
2024-08-14 11:24:05 +0300

    Starting dev

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,8 @@
 import 'CSS\index.css';
 
 function component() {
+
     
-    const element = document.createElement('div');
-    element.innerHTML = 'Hello World!';
-    return element;
     
 }
 
