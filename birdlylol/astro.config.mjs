2f6a21c8063c3f08356d9763cb39279d7f031218
Ruiyan Tang <132647254+Firebirbionmahtavakoodaajees@users.noreply.github.com>
2026-08-20 13:58:46 +0300

    Remove Vercel adapter from Astro config

--- birdlylol/astro.config.mjs
+++ birdlylol/astro.config.mjs
@@ -2,10 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()],
-  adapter: vercel()
+  integrations: [sitemap()]
 });
